e49ec63000fb9d0e3c189324068115d42a1739d4
Marc S <psx2pal@hotmail.de>
2013-12-09 12:49:44 +0100

    first commit data

---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Release name
+PRODUCT_RELEASE_NAME := n8000
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/n8000/full_n8000.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := n8000
+PRODUCT_NAME := cm_n8000
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-N8000
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-N8013 TARGET_DEVICE=GT-N8000 BUILD_FINGERPRINT="samsung/p4noterfxx/p4noterf:4.1.2/JZO54K/N8000XXCME2:user/release-keys" PRIVATE_BUILD_DESC="samsung/p4noterfxx/p4noterf:4.1.2/JZO54K/N8000XXCME2:user/release-keys"
